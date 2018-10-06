636b3bdc50b8306fd72fcfeb7b37d179bad16bc2
ghafran <ghafranabbas@gmail.com>
2018-10-06 15:50:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,47 @@
-var express = require( 'express')
-var app = express()
-var cf_app = require( './app/vcap_application')
-var cf_svc = require( './app/vcap_services')
+var express = require('express');
+var app = express();
+var cf_app = require('./app/vcap_application');
+var cf_svc = require('./app/vcap_services');
+const {
+    Client
+} = require('pg');
 
-app.set( 'views', __dirname + '/views')
-app.set( 'view engine', 'jade')
-app.use( express.static( __dirname + '/public'))
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
 
-app.get( '/', function ( req, res) {
-  res.render( 'pages/index', {
-    app_environment:    app.settings.env,
-    application_name:   cf_app.get_app_name(),
-    app_uris:           cf_app.get_app_uris(),
-    app_space_name:     cf_app.get_app_space(),
-    app_index:          cf_app.get_app_index(),
-    app_mem_limits:     cf_app.get_app_mem_limits(),
-    app_disk_limits:    cf_app.get_app_disk_limits(),
-    service_label:      cf_svc.get_service_label(),
-    service_name:       cf_svc.get_service_name(),
-    service_plan:       cf_svc.get_service_plan()
-  })
-})
+app.get('/', function(req, res) {
+    res.render('pages/index', {
+        app_environment: app.settings.env,
+        application_name: cf_app.get_app_name(),
+        app_uris: cf_app.get_app_uris(),
+        app_space_name: cf_app.get_app_space(),
+        app_index: cf_app.get_app_index(),
+        app_mem_limits: cf_app.get_app_mem_limits(),
+        app_disk_limits: cf_app.get_app_disk_limits(),
+        service_label: cf_svc.get_service_label(),
+        service_name: cf_svc.get_service_name(),
+        service_plan: cf_svc.get_service_plan()
+    });
+});
 
-app.listen( process.env.PORT || 4000)
+app.get('/db', function(req, res) {
+
+    var client = new Client();
+    client.connect();
+    client.query('SELECT $1::text as message', ['Hello world!'], (err, data) => {
+        if (err) {
+            res.send(err.message);
+        } else {
+            res.send(data.rows[0].message);
+        }
+        client.end();
+    });
+});
+
+app.get('/env', function(req, res) {
+
+    res.send(process.env);
+});
+
+app.listen(process.env.PORT || 4000);
